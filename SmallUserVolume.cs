0f2e428c2dab0c9bc5047597ee6226557c076c2d
KeafyIsHere <58978401+KeafyIsHere@users.noreply.github.com>
2021-05-23 08:32:56 +0100

    Update SmallUserVolume.cs

--- SmallUserVolume.cs
+++ SmallUserVolume.cs
@@ -10,23 +10,23 @@ namespace SmallUserVolume
         {
             Transform UV = GameObject.Find("UserInterface/QuickMenu/UserInteractMenu/User Volume").transform;
             UV.FindChild("VolumeText").gameObject.SetActive(false);
-            GameObject VD = UV.FindChild("VolumeDisplay").gameObject;
-            GameObject VS = UV.FindChild("VolumeSlider").gameObject;
-            GameObject RaV = UV.FindChild("Raise").gameObject;
-            GameObject LV = UV.FindChild("Lower").gameObject;
-            GameObject ReV = UV.FindChild("Reset").gameObject;
+            RectTransform VD = UV.FindChild("VolumeDisplay").GetComponent<RectTransform>();
+            RectTransform VS = UV.FindChild("VolumeSlider").GetComponent<RectTransform>();
+            RectTransform RaV = UV.FindChild("Raise").GetComponent<RectTransform>();
+            RectTransform LV = UV.FindChild("Lower").GetComponent<RectTransform>();
+            RectTransform ReV = UV.FindChild("Reset").GetComponent<RectTransform>();
 
-            VD.GetComponent<RectTransform>().sizeDelta = new Vector2(840f, 37.71f);
+            VD.sizeDelta = new Vector2(840f, 37.71f);
             VD.transform.localPosition = new Vector3(-210f, VD.transform.localPosition.y);
-            VS.GetComponent<RectTransform>().sizeDelta = new Vector2(840f, 160f);
+            VS.sizeDelta = new Vector2(840f, 160f);
             VS.transform.localPosition = new Vector3(-210f, VS.transform.localPosition.y);
-            RaV.GetComponent<RectTransform>().sizeDelta = new Vector2(210f, 210f);
+            RaV.sizeDelta = new Vector2(210f, 210f);
             RaV.GetComponentInChildren<Text>().text = "+";
             RaV.transform.localPosition = new Vector3(+105f, RaV.transform.localPosition.y);
-            LV.GetComponent<RectTransform>().sizeDelta = new Vector2(210f, 210f);
+            LV.sizeDelta = new Vector2(210f, 210f);
             LV.GetComponentInChildren<Text>().text = "-";
             LV.transform.localPosition = new Vector3(-525f, LV.transform.localPosition.y);
-            ReV.GetComponent<RectTransform>().sizeDelta = new Vector2(420f, 210f);
+            ReV.sizeDelta = new Vector2(420f, 210f);
             ReV.transform.localPosition = new Vector3(-210f, ReV.transform.localPosition.y);
         }
     }
