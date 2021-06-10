f2572cf13b718851d81347cd0d4dcd911a483940
Boppr <34575606+RealBoppr@users.noreply.github.com>
2021-06-10 23:12:54 +1000

    Fixed OnUIManagerInit

--- SmallUserVolume.cs
+++ SmallUserVolume.cs
@@ -1,4 +1,5 @@
-﻿using MelonLoader;
+using System.Collections;
+using MelonLoader;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,28 +7,37 @@ namespace SmallUserVolume
 {
     public class Class : MelonMod
     {
-        public override void VRChat_OnUiManagerInit()
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(CheckUIManager());
+        }
+        private IEnumerator CheckUIManager()
+        {
+            while (VRCUiManager.prop_VRCUiManager_0 == null) { yield return null; }
+            OnUIManagerInit();
+        }
+        public void OnUIManagerInit()
         {
             Transform UV = GameObject.Find("UserInterface/QuickMenu/UserInteractMenu/User Volume").transform;
             UV.FindChild("VolumeText").gameObject.SetActive(false);
-            RectTransform VD = UV.FindChild("VolumeDisplay").GetComponent<RectTransform>();
-            RectTransform VS = UV.FindChild("VolumeSlider").GetComponent<RectTransform>();
-            RectTransform RaV = UV.FindChild("Raise").GetComponent<RectTransform>();
-            RectTransform LV = UV.FindChild("Lower").GetComponent<RectTransform>();
-            RectTransform ReV = UV.FindChild("Reset").GetComponent<RectTransform>();
+            Transform VD = UV.FindChild("VolumeDisplay");
+            Transform VS = UV.FindChild("VolumeSlider");
+            Transform RaV = UV.FindChild("Raise");
+            Transform LV = UV.FindChild("Lower");
+            Transform ReV = UV.FindChild("Reset");
 
-            VD.sizeDelta = new Vector2(840f, 37.71f);
-            VD.transform.localPosition = new Vector3(-210f, VD.transform.localPosition.y);
-            VS.sizeDelta = new Vector2(840f, 160f);
-            VS.transform.localPosition = new Vector3(-210f, VS.transform.localPosition.y);
-            RaV.sizeDelta = new Vector2(210f, 210f);
+            VD.GetComponent<RectTransform>().sizeDelta = new Vector2(840f, 37.71f);
+            VD.localPosition = new Vector3(-210f, VD.localPosition.y, VD.localPosition.z);
+            VS.GetComponent<RectTransform>().sizeDelta = new Vector2(840f, 160f);
+            VS.localPosition = new Vector3(-210f, VS.localPosition.y, VS.localPosition.z);
+            RaV.GetComponent<RectTransform>().sizeDelta = new Vector2(210f, 210f);
             RaV.GetComponentInChildren<Text>().text = "+";
-            RaV.transform.localPosition = new Vector3(+105f, RaV.transform.localPosition.y);
-            LV.sizeDelta = new Vector2(210f, 210f);
+            RaV.localPosition = new Vector3(+105f, RaV.localPosition.y, RaV.localPosition.z);
+            LV.GetComponent<RectTransform>().sizeDelta = new Vector2(210f, 210f);
             LV.GetComponentInChildren<Text>().text = "-";
-            LV.transform.localPosition = new Vector3(-525f, LV.transform.localPosition.y);
-            ReV.sizeDelta = new Vector2(420f, 210f);
-            ReV.transform.localPosition = new Vector3(-210f, ReV.transform.localPosition.y);
+            LV.localPosition = new Vector3(-525f, LV.localPosition.y, LV.localPosition.z);
+            ReV.GetComponent<RectTransform>().sizeDelta = new Vector2(420f, 210f);
+            ReV.localPosition = new Vector3(-210f, ReV.localPosition.y, ReV.localPosition.z);
         }
     }
 }
